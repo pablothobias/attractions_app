fe6aa5e5cec16e0fec7c634bf88308966bc8e10e
Pablo Thobias <pablo.carminatti@newsela.com>
2024-01-03 10:54:23 -0300

    feature(AttractionsApp): adding nested serializers to attraction serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,13 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer,
+    ModelSerializer,
+    SerializerMethodField,
+)
 from core.models import Attraction
+from address.api.serializers import AddressSerializer
+from reviews.api.serializers import ReviewSerializer
 
 
 class AttractionSerializer(ModelSerializer):
+    address = AddressSerializer(many=False)
+    reviews = ReviewSerializer(many=True)
+    name_id = SerializerMethodField()
+
     class Meta:
         model = Attraction
         fields = (
             "id",
+            "name_id",
             "name",
             "description",
             "image",
+            "address",
+            "reviews",
         )
+
+    def get_name_id(self, obj):
+        return obj.name + " - " + str(obj.id)
