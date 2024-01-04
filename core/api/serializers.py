481cd41515d2a28d74adf55d16bdd2e38cd126a5
Pablo Thobias <pablo.carminatti@newsela.com>
2024-01-04 10:26:09 -0300

    feature(AttractionsApp): implementing capability of adding attractions with nested address

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -5,12 +5,13 @@ from rest_framework.serializers import (
 )
 from core.models import Attraction
 from address.api.serializers import AddressSerializer
+from address.models import Address
 from reviews.api.serializers import ReviewSerializer
 
 
 class AttractionSerializer(ModelSerializer):
     address = AddressSerializer(many=False)
-    reviews = ReviewSerializer(many=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
     name_id = SerializerMethodField()
 
     class Meta:
@@ -24,6 +25,17 @@ class AttractionSerializer(ModelSerializer):
             "address",
             "reviews",
         )
+
+    def insert_address(self, attraction, address):
+        address = Address.objects.create(**address)
+        attraction.address.add(address)
+
+    def create(self, validated_data):
+        address = validated_data["address"]
+        del validated_data["address"]
+        attraction = Attraction.objects.create(**validated_data)
+        self.insert_address(attraction, address)
+        return attraction
 
     def get_name_id(self, obj):
         return obj.name + " - " + str(obj.id)
